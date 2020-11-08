9d6e5517b383f1e2904e059483d122c32f202324
Helberth Lucas <helberthlucas14@gmail.com>
2020-11-08 12:41:56 -0300

    Create auth module

--- frontend-web/src/pages/Auth/componets/Login/index.tsx
+++ frontend-web/src/pages/Auth/componets/Login/index.tsx
@@ -1,24 +1,42 @@
 import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    username: string;
+    password: string;
+}
+
 const Login = () => {
+
+    const { register, handleSubmit } = useForm<FormData>();
+    
+    const onSubmit = (data: FormData) => {
+        console.log(data)
+    }
+
     return (
         <AuthCard title="login">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
                     className="form-control input-base margin-bottom-30"
                     placeholder="Email"
+                    name="username"
+                    ref={register}
                 />
 
                 <input
                     type="password"
                     className="form-control input-base"
                     placeholder="Senha"
+                    name="password"
+                    ref={register}
                 />
+
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha ?
                 </Link>
@@ -27,11 +45,11 @@ const Login = () => {
                 </div>
                 <div>
                     <span className="not-registered">
-                        Não tenho Cadastro? 
+                        Não tenho Cadastro?
                     </span>
 
                     <Link to="/admin/auth/register" className="login-link-register">
-                       CADASTRAR
+                        CADASTRAR
                     </Link>
                 </div>
             </form>
