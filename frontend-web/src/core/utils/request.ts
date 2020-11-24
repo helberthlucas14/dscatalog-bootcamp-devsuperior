e085a9c6e925666bb1c80e3545ad951103f1b6e2
Helberth Lucas <helberthlucas14@gmail.com>
2020-11-24 20:06:07 -0300

    Add env vars

--- frontend-web/src/core/utils/request.ts
+++ frontend-web/src/core/utils/request.ts
@@ -16,7 +16,7 @@ type LoginData = {
     password: string;
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 axios.interceptors.response.use(function (response) {
     return response;
@@ -25,7 +25,7 @@ axios.interceptors.response.use(function (response) {
         history.push('/auth/login');
         logout();
     }
-    
+
     return Promise.reject(error);
 });
 
